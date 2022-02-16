542bd6c05c287946781acabb3fe42089016d0153
Ryan Peoples <peoplesjermaine@gmail.com>
2022-02-16 14:34:26 -0700

    enqueue done

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -5,17 +5,27 @@ class Queue {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.length = 0;
     }
 
     enqueue(val) {
         // Add node to end of queue (linked list)
+        let newNode = new SinglyLinkedNode(val)
+        this.length++;
+        if(!this.head){
+            this.head=newNode;
+            this.tail=newNode;
+        };
+        this.tail.next = newNode;
+        this.tail = newNode;
+        return this.length;
 
         // Write your hypothesis on the time complexity of this method here
     }
 
     dequeue() {
         // Remove node from front of queue (linked list)
-        
+
         // Write your hypothesis on the time complexity of this method here
     }
 
